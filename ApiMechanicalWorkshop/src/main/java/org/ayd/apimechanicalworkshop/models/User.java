165cb011d60f5b02d362dcfcbaff9fce7d78ab63
luisbaquiax <luis.baquiax1995@gmail.com>
2025-08-20 01:55:24 -0600

    add lombock dependece

--- ApiMechanicalWorkshop/src/main/java/org/ayd/apimechanicalworkshop/models/User.java
+++ ApiMechanicalWorkshop/src/main/java/org/ayd/apimechanicalworkshop/models/User.java
@@ -1,9 +1,15 @@
 package org.ayd.apimechanicalworkshop.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
@@ -21,108 +27,4 @@ public class User {
     private String updatedAt;
     private boolean twoFactorAuth;
     private String twoFactorMethod;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getRolId() {
-        return rolId;
-    }
-
-    public void setRolId(int rolId) {
-        this.rolId = rolId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(String updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public boolean isTwoFactorAuth() {
-        return twoFactorAuth;
-    }
-
-    public void setTwoFactorAuth(boolean twoFactorAuth) {
-        this.twoFactorAuth = twoFactorAuth;
-    }
-
-    public String getTwoFactorMethod() {
-        return twoFactorMethod;
-    }
-
-    public void setTwoFactorMethod(String twoFactorMethod) {
-        this.twoFactorMethod = twoFactorMethod;
-    }
 }
