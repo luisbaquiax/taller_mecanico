619f45197ec5bd2cd90545f7b933f2770cdeb984
luisbaquiax <luis.baquiax1995@gmail.com>
2025-08-20 18:37:53 -0600

    java21

--- ApiMechanicalWorkshop/src/test/java/org/luisbaquiax/apimechanicalworkshop/ApiMechanicalWorkshopApplicationTests.java
+++ ApiMechanicalWorkshop/src/test/java/org/luisbaquiax/apimechanicalworkshop/ApiMechanicalWorkshopApplicationTests.java
@@ -1,4 +1,4 @@
-package org.ayd.apimechanicalworkshop;
+package org.luisbaquiax.apimechanicalworkshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
