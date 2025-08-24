d95bcac824c7e9cf4e10b17ba7e7b0ed3869d3c0
luisbaquiax <luis.baquiax1995@gmail.com>
2025-08-24 02:48:21 -0600

    add✨: login in frontend app

---
+++ AppMechanicalWorkshop/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login,
+        title: 'Login'
+    }
+];
---
+++ AppMechanicalWorkshop/src/app/components/login/login.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Login {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  hide = signal(true);
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const username = this.loginForm.get('username')?.value;
+      const password = this.loginForm.get('password')?.value;
+      console.log('Username:', username);
+      console.log('Password:', password);
+    }
+  }
+}
